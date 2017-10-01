59995d98335943b70ebd8787340e50bf47a7e4a4
Jason Smyth <smyt0058@algonquinlive.com>
2017-09-30 22:40:27 -0400

    Project changed to Android 7.1.1

---
+++ HiLoGame/app/src/main/java/com/algonquincollege/smyt0058/hilogame/AboutDialogFragment.java
@@ -0,0 +1,36 @@
+package com.algonquincollege.smyt0058.hilogame;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+/**
+ * Created by Jason on 2017-09-27.
+ */
+
+public class AboutDialogFragment extends DialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the Builder class for convenient dialog construction
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        // Decorate our About dialog
+        builder.setTitle(R.string.action_about)
+                //TODO replace with your name + userID
+                .setMessage(R.string.author)
+                .setCancelable(false)
+                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+
+        // Create the AlertDialog object and return it
+        return builder.create();
+        //return super.onCreateDialog(savedInstanceState);
+
+    }
+}
